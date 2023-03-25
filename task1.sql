79a0fdf729a6594ac76a5288e34a000fa826ed4c
Yurii Plantus <yurii_plantus@epam.com>
2023-03-25 14:33:58 +0200

    added new task

--- task1.sql
+++ task1.sql
@@ -14,3 +14,12 @@ where FirstName like 'A%'
 select count(employee_id)   
 from employees 
 where city  ='London'
+
+--Show the list of first, last names and ages of the employees whose age is greater than average age of all employees. The result should be sorted by last name
+
+select first_name, employees.title, last_name, extract(year  from  age(birth_date ))  age 
+from employees 
+where extract (year  from  age(birth_date ))>
+(select  avg( extract (year  from  age(birth_date )) ) 
+from employees )
+order by last_name desc  
